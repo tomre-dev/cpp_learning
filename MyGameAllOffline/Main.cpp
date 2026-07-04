4b7ab81d3da3b86fa49b41add23d4bcf8dd7b732
Tommaso Re <re.tommaso.5@gmail.com>
2026-07-04 12:54:04 +0000

    new minigame so cool

---
+++ MyGameAllOffline/Main.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+
+
+
+class Player
+{
+public:
+	char name = 'A';// i initialized because if i didnt it was an error so yes
+	int hp = 100;
+	int strenght = 15;
+	int x = 0;
+	int y = 0;
+public:
+
+	void Attack(Player& target)
+	{
+		if (target.x == x && target.y == y)
+		{
+			target.hp -= strenght;
+			std::cout << name << " Attacked " << target.name << " and hit him with a " << strenght << " hp punch" << std::endl;
+		}
+		else
+		{
+			std::cout << name << " Tried to attack " << target.name << " but he was to far" << std::endl;
+		}
+	}
+
+	void Move(int nx , int ny)
+	{
+		x += nx;
+		y += ny;
+
+		std::cout << name << " Moved To " << " X " << x << " Y " << y << std::endl;
+	}
+
+	void TellStats()
+	{
+		std::cout << "--------------STATS------------------" << std::endl;
+		std::cout << "NAME: " << name << std::endl;
+		std::cout << "HP: " << hp << std::endl;
+		std::cout << "STRENGHT: " << strenght << std::endl;
+		std::cout << "X: " << x << std::endl;
+		std::cout << "Y: " << y << std::endl;
+		std::cout << "-------------------------------------" << std::endl;
+
+	}
+
+	void take_input(Player& target)
+	{
+		int input = 0;
+		std::cout << name << " what are you going to do? " << std::endl;
+		std::cout << " 1 - Attack - " << std::endl;
+		std::cout << " 2 - Move - " << std::endl;
+		std::cin >> input;
+		if (input == 1)
+		{
+			Attack(target);
+		}
+		if (input == 2)
+		{
+			int input = 0;
+			std::cout << "Where you wanna move " << std::endl;
+			std::cout << " 1 - Right ->" << std::endl;
+			std::cout << " 2 - Left  <- " << std::endl;
+			std::cout << " 3 - up " << std::endl;
+			std::cout << " 4 - down " << std::endl;
+			std::cin >> input;
+
+			if (input == 1)
+			{
+				Move(1, 0);
+			}
+			else if (input == 2)
+			{
+				Move(-1, 0);
+			}
+			else if (input == 3)
+			{
+				Move(0, 1);
+			}
+			else if (input == 4)
+			{
+				Move(0, -1);
+			}
+
+		}
+
+	}
+
+
+};
+
+
+
+
+
+
+int main()
+{
+	Player player1;
+	Player player2;
+	
+	std::cout << "what is the name of the first PLAYER.. " << std::endl;
+	std::cin >> player1.name;
+
+	std::cout << "what is the name of the second PLAYER.. " << std::endl;
+	std::cin >> player2.name;
+
+	std::cout << "-------------------------" << std::endl;
+	while (player1.hp > 0 and player2.hp > 0)
+	
+	{
+		std::cout << "---------ACTION-----------" << std::endl;
+
+		player1.take_input(player2);
+
+		if (not (player2.hp > 0))
+		{
+			break;
+		}
+
+		player2.take_input(player1);
+
+		std::cout << "---------STASTS-----------" << std::endl;
+
+		player1.TellStats();
+		player2.TellStats();
+	}
+
+	std::cout << "--------FINISH----------" << std::endl;
+
+
+}
---
+++ Pointers&&reference/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define LOG(x) std:::cout << x << std::endl
+
+int main()
+{
+	int var = 8;
+	void* ptr = &var;
+
+
+}

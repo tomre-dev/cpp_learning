1d35386b42f1091ef61ec2dbccd00c2e5c3e733a
Tommaso Re <re.tommaso.5@gmail.com>
2026-07-08 12:57:35 +0000

    static update

---
+++ Static/Main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+//
+//int s_Variable = 10;
+//
+//
+//int main()
+//{
+//	std::cout << s_Variable << std::endl;
+//}
+//
+//struct Entity
+//{
+//	static int x, y;
+//
+//	void print()
+//	{
+//		std::cout << x << " , " << y << std::endl;
+//	}
+//};
+//
+//
+//int Entity::x;
+//int Entity::y;
+//
+//int main()
+//{
+//	Entity pig;
+//	pig.x = 2;
+//	pig.y = 3;
+//
+//
+//	Entity cow;
+//	cow.x = 5;
+//	cow.y = 8;
+//
+//	pig.print();
+//	cow.print();
+//}
+
+void Function()
+{
+	static int c = 0;
+	c++;
+	std::cout << c << std::endl;
+}
+
+
+int main()
+{
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+}

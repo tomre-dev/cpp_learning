438a3ee1213c712a32e64900d7c6e17bf4711646
Tommaso Re <re.tommaso.5@gmail.com>
2026-07-10 12:55:28 +0000

    idkengine 3

--- MyEngine/Main.cpp
+++ MyEngine/Main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 /*
-yeah this will be my engine i will programm it oall offline without any tutorial because i want and if you dont like it why are you even here?
-go programm yourself this will be the !best engine (understand the reference!!!)
-the name of the engine will be idk and is opensource
+opensource
 -----------------------------------------------------------------------------------
 
                   IDK ENGINE 2
@@ -11,7 +9,7 @@ the name of the engine will be idk and is opensource
 */
 float Mysqrt(float number)
 {
-	float guess = number / 2.0f;
+    float guess = number / 2.0f;//if was an in i would have done >> 1 its like divide by 2 but faster! you move the bits undertstand?
 
 	for (int i = 0; i < 10; i++)
 	{
@@ -23,7 +21,6 @@ float Mysqrt(float number)
 
 
 
-//update 2.0 introduces a new way to think now its all different but at least compile!
 
 struct Vec3
 {
@@ -85,7 +82,7 @@ struct Vec3
     {
         return (Mysqrt(X * X + Y * Y + Z * Z));
     }
-    void Resize(float n)
+    void Multiply(float n)
     {
         X *= n;
         Y *= n;
@@ -105,7 +102,6 @@ struct Transform
 
 struct Form
 {
-    //ok for now only sphere and i know this struct is bad but i try to make it work
     float radius;
 
     Form(float n)
@@ -118,31 +114,66 @@ struct Form
     }
 };
 
+struct Velocity
+{
+    Vec3 speed;
+
+    Velocity()
+    {
+        speed.Change(-0.50f, 0.0f, 0.0f);
+    }
+    Velocity(float x , float y , float z)
+    {
+        speed.Change(x , y , z);
+    }
+};
+
+struct Gravity
+{
+    float currentgravity;
+    float constantgravity;
+
+
+    Gravity(float n , float c)
+    {
+        constantgravity = n;
+        currentgravity = c;
+    }
+    Gravity()
+    {
+        constantgravity = 9.81;
+        currentgravity = 0;
+
+    }
+    void UpdateGravity()
+    {
+        currentgravity += constantgravity;
+    }
+
+    
+};
+
+
+
+struct RigidBody
+{
+    Velocity velocity;
+    Gravity gravity;
+};
 
 class Object
 {
 public:
     Transform transform;
     Form form;
+    RigidBody rigidbody;
+
 };
 
 
-//ok so for creating 3d im watching a tutorial online i admit its difficult i will only watch how to create a rigidbody if a tut exist so i will upgrade
 
-/*
 
-ok i informed myself i have a position that is what matter now the simplest thing i can do is a sphere because
-i can understand if they overlap by just seiing if the center of sphere 1 and 2 are like 10meters away 
-if the radius of every sphere is 6 they are touching if the distance between the 2 centers  is less than the some of 
-theyre radius they are overlapping and this im sure it works with a sphere of 8 r and 2 im pretty sure 
-yeah its basic math im scared for the future but not now
-so its like 
-
-distance of two sphere is less than radius 1 + radius 2
-
-*/
-
-float Distance(Object obj1, Object obj2)
+float Distance(Object& obj1, Object& obj2)
 {
     //ok now we take the difference of the axis so we know like we get x3 y0 z0 we know that the object to reach is 3 steps to x 0 on y 0 on z so we take it 
     //and trow it into my fntastic sqrt and we have the distance 
@@ -165,7 +196,7 @@ float Distance(Object obj1, Object obj2)
 
 }
 
-bool IsColliding(Object obj1 , Object obj2)
+bool IsColliding(Object& obj1 , Object& obj2)
 {
     
     if (obj1.form.radius + obj2.form.radius >= Distance(obj1, obj2))
@@ -180,26 +211,53 @@ bool IsColliding(Object obj1 , Object obj2)
 }
 
 
-
-//omg i thought that i needed like at least 4 month from now to build a miniengine but i did it i understood that your never ready but always ready to try its crazy
-
+//ok so for idkengine 3 i want to make the things i remenber i unity first there was a function you could call at the start and then one every frame
+//the rigidbody with speed
 
 int main()
 {
+    //called at the start
+
     Object haunter;
+    haunter.transform.position.Change(10, 0, 0);
+    haunter.form.radius = 1;
 
     Object pikachu;
-    pikachu.transform.position.Change(10.34, 23.482, 8.132);
-    pikachu.form.radius = 7.53;
-    haunter.transform.position.Change(4.01, 13.32, -0.328);
-    haunter.form.radius = 12.902;
+    pikachu.transform.position.Change(0, 0, 0);
+    pikachu.form.radius = 1;
+
+    Object global;
+    
+    while (true)//called every frame
+    {
+        float floor_height = -20;
+        //simple gravity nothing to crazy
+        
+        haunter.transform.position.Sub(0.0f, global.rigidbody.gravity.currentgravity , 0.0f);
+        pikachu.transform.position.Sub(0.0f, global.rigidbody.gravity.currentgravity, 0.0f);
+        global.rigidbody.gravity.UpdateGravity();
 
 
-    bool info = IsColliding(pikachu, haunter);
 
-    std::cout << info << std::endl;
+        //i move haunter and i think with the rotation i can do cool things even with scalle now i can makes sense of those in a future update i will use them
+        haunter.transform.position.Sum(haunter.rigidbody.velocity.speed);
+      
 
-}//omg it runs i fought for bugs but there werent many i am very happy really
-//now i know haunter and pikachu collided i am very happy really now i can undertand if something collide it nedds to be a sphere but its ok 
-//now i wanna make something more cool like i can simulate a launch of a missile and i create an area and if collide with the sphere of pikachu he die muahahahah evil plan 
-//its crazy i started this engine as a joke i know is not that much for everyone that see this but i am happy!
+        //little bug dei collide more than 1 time but fine
+        if (IsColliding(pikachu, haunter))
+        {
+            haunter.rigidbody.velocity.speed.Multiply(-1);
+        }
+
+        //floor 
+        if (haunter.transform.position.Y < floor_height)
+        {
+            haunter.transform.position.Y = floor_height;
+            haunter.rigidbody.gravity.currentgravity = 0;
+        }
+
+        
+    }
+
+}
+

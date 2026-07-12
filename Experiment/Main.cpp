580e22dd5accd4d59b75716c9d4740429500945a
Tommaso Re <re.tommaso.5@gmail.com>
2026-07-12 12:06:01 +0000

    experiments

---
+++ Experiment/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+//experiment
+
+//with new we will allocate 1 gb in a row on the heap i wanna see what the operating system do
+
+int main()
+{
+	int array1[134217728];
+
+
+	std::cin.get();
+}
+
+
+
+
+
+
+
+
+
+
+
--- VirtualFunctions/Main.cpp
+++ VirtualFunctions/Main.cpp
@@ -1,1 +1,48 @@
-//coming soon
+#include <iostream>
+#include <string>
+
+class Entity
+{
+public:
+	virtual std::string GetName()
+	{
+		return "Entity";
+	}
+};
+
+class Player : public Entity
+{
+private:
+	std::string N_name;
+public:
+	Player(const std::string& name)
+		: N_name(name) {}
+
+	std::string GetName() 
+	{
+		return N_name;
+	}
+};
+
+
+void PrintName(Entity* entity)
+{
+	std::cout << entity->GetName() << std::endl;
+}
+
+int main() 
+{
+	Entity* e = new Entity;
+	std::cout << e->GetName() << std::endl;
+	PrintName(e);
+
+
+
+	Player* p = new Player("Tommaso");
+	std::cout << p->GetName() << std::endl;
+	PrintName(p);
+
+	
+
+
+}
